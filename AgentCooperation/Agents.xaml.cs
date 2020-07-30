f8a0614c027632924f61b58416e4b5c9ee27146b
Bartosz Nawrot <banaw11@gmail.com>
2020-07-30 23:10:20 +0200

    Dodaj pliki projektów.

---
+++ AgentCooperation/Agents.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace AgentCooperation
+{
+    /// <summary>
+    /// Logika interakcji dla klasy Agents.xaml
+    /// </summary>
+    public partial class Agents : Window
+    {
+        private static List<Agent> agents;
+        public Agents()
+        {
+            InitializeComponent();
+            LoadAgents();
+            AgentsGridView.ItemsSource = agents;
+        }
+
+        private void LoadAgents()
+        {
+            agents = SqliteDataAccess.LoadAgents();
+        }
+    }
+
+    public class Agent
+    {
+        public string Agent_Code { get; set; }
+        public string Agent_Name { get; set; }
+        public string Working_Area { get; set; }
+        public float Commission { get; set; }
+        public string Phone_No { get; set; }
+        public string Country { get; set; }
+
+    }
+}
---
+++ AgentCooperation/SqliteDataAccess.cs
@@ -0,0 +1,67 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgentCooperation
+{
+    public class SqliteDataAccess
+    {
+        private static string Id = "Default";
+        private static IDbConnection dbConnection = new SQLiteConnection(LoadConnectionString());
+        public static List<Agent> LoadAgents()
+        {
+            using (dbConnection) 
+            {
+                var output = dbConnection.Query<Agent>("SELECT * From AGENTS", new DynamicParameters());
+                return output.ToList();
+            }
+        }
+
+        public static void AddAgent(Agent agent)
+        {
+            using (dbConnection)
+            {
+                dbConnection.Execute
+                    ("If NOT EXIST ( SELECT * From AGENTS" +
+                                    "WHERE AGENT_CODE = @Agent_Code )" +
+                    "BEGIN" +
+                        "INSERT INTO AGENTS (AGENT_CODE, AGENT_NAME, WORKING_AREA, COMMISSION, PHONE_NUMBER, COUNTRY)" +
+                        "VALUES(@Agent_Code, @Agent_Name, @Working_Area, @Commision, @Phone_NO, @Country)"+
+                    "END", agent);
+            }
+        }
+
+        public static void RemoveAgent(Agent agent)
+        {
+            using (dbConnection)
+            {
+                dbConnection.Execute
+                    ("IF EXISTS (SELECT * FROM AGENTS" +
+                                "WHERE AGENT_CODE = @Agent_Code)" +
+                     "BEGIN" +
+                        "DELETE From AGENTS WHERE AGENT_CODE = @Agent_Code" +
+                      "END", agent);
+            }
+        }
+
+        public static List<Agent> SearchAgents(string condition, string value)
+        {
+            using (dbConnection)
+            {
+                var output = dbConnection.Query<Agent>(("SELECT * From AGENTS WHERE {0} = {1}", condition, value).ToString(), new DynamicParameters());
+                return output.ToList();
+            }
+        }
+
+        private static string LoadConnectionString()
+        {
+            return ConfigurationManager.ConnectionStrings[Id].ConnectionString;
+        }
+    }
+}
